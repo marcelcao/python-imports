62075dabec0e87818d8a0725c40f60c83ffa82bb
Mara Caoile <mara.caoile@gmail.com>
2023-10-11 19:38:19 -0500

    refactored and working

--- index.py
+++ index.py
@@ -1,16 +1,24 @@
-from appliances.kitchen import Dishwasher
-from appliances.laundry import Dryer
-from appliances.laundry import Washer
-from appliances.kitchen.utility import Refrigerator
+from appliances import DishWasher
+from appliances import CoffeeMaker
+from appliances import Dryer
+from appliances import Washer
+from appliances import Refrigerator
+from appliances import CanOpener
 
-whirlpool_dishwasher = Dishwasher("black")
+whirlpool_dishwasher = DishWasher("black")
 whirlpool_dishwasher.wash_dishes()
 
 samsung_washer = Washer("red")
+samsung_washer.wash_clothes("delicates")
+
 samsung_dryer = Dryer("red", "gas")
+samsung_dryer.dry_clothes("high")
 
 lg_fridge = Refrigerator("stainless")
 lg_fridge.make_ice()
 
 mr_coffee = CoffeeMaker("white")
 mr_coffee.make_coffee()
+
+open_can = CanOpener("green")
+open_can.open_can()
